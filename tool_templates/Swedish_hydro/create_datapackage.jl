790ff349699d71deea9ca21308276b33654c8d1b
Manuel Marin <manuelma@kth.se>
2018-03-23 12:19:30 +0100

    add Swedish hydro example

---
+++ tool_templates/Swedish_hydro/create_datapackage.jl
@@ -0,0 +1,14 @@
+using SpineData
+using JSON
+
+dc = JSON.parsefile("input/data_connection.json")
+@unpack(dc, path)
+
+pkg_desc = infer_datapackage_descriptor(path)
+
+set_primary_key!(pkg_desc, "Plants", ["station_index"])
+
+add_foreign_key!(pkg_desc, "Plants", ["downstream"], "Plants", ["station_index"])
+add_foreign_key!(pkg_desc, "Constraints", ["constraint_station"], "Plants", ["station_name"])
+
+save_datapackage_to_disk(pkg_desc, path)
