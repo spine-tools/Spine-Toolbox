f6dc70b4c6790fe525aecc5b2316b5f92dafb138
Manuel Marin <manuelma@kth.se>
2018-05-13 19:54:01 +0200

    Add custom QDialog to interact with Treeview in DataStore.

---
+++ spinetoolbox/widgets/custom_qdialog.py
@@ -0,0 +1,72 @@
+#############################################################################
+# Copyright (C) 2017 - 2018 VTT Technical Research Centre of Finland
+#
+# This file is part of Spine Toolbox.
+#
+# Spine Toolbox is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#############################################################################
+
+"""
+QDialog that can be programatically populated with many options.
+Originally intended to be used within DataStoreForm
+
+:author: Manuel Marin <manuelma@kth.se>
+:date:   13.5.2018
+"""
+
+from PySide2.QtWidgets import QDialog, QFormLayout, QLineEdit, QDialogButtonBox, QComboBox
+from PySide2.QtCore import Slot, Qt
+
+class CustomQDialog(QDialog):
+    """A class to create custom forms with several line edits and comboboxes."""
+    def __init__(self, parent=None, title="", **kwargs):
+        """Initialize class
+
+        Args:
+            parent (QWidget): the parent of this dialog, needed to center it properly
+            title (str): window title
+            kwargs (dict): keys to use when collecting the answer in output dict.
+                Values are either placeholder texts or combobox lists.
+        """
+        super().__init__(parent)
+        self.input = dict()
+        self.answer = dict()
+        self.setWindowTitle(title)
+        form = QFormLayout(self)
+        for key,value in kwargs.items():
+            if isinstance(value, str): # line edit
+                input_ = QLineEdit(self)
+                input_.setPlaceholderText(value)
+            elif isinstance(value, list): # combo box
+                input_ = QComboBox(self)
+                input_.addItems(value)
+            self.input[key] = input_
+            form.addRow(input_)
+        button_box = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+        button_box.accepted.connect(self.save_and_accept)
+        button_box.rejected.connect(self.reject)
+        form.addRow(button_box)
+
+    @Slot(name="save_and_accept")
+    def save_and_accept(self):
+        """Collect answer in output dict and accept"""
+        for key,value in self.input.items():
+            if isinstance(value, QLineEdit):
+                self.answer[key] = value.text()
+            elif isinstance(value, QComboBox):
+                self.answer[key] = {
+                    'text': value.currentText(),
+                    'index': value.currentIndex()
+                }
+        self.accept()
