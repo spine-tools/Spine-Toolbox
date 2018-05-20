9cea6025fcd266d8defac10faf5517a9cb771930
Manuel Marin <manuelma@kth.se>
2018-05-20 19:28:18 +0200

    Julia command line can now handle a mini-history of issued commands

--- spinetoolbox/widgets/custom_qlineedit.py
+++ spinetoolbox/widgets/custom_qlineedit.py
@@ -27,7 +27,8 @@ Class for a custom QLineEdit for the lineEdit_process_command.
 import logging
 from PySide2.QtWidgets import QLineEdit
 from PySide2.QtCore import Signal, Slot, Qt
-
+from pathlib import Path
+import os
 
 class CustomQLineEdit(QLineEdit):
     """Custom QLineEdit class.
@@ -43,11 +44,22 @@ class CustomQLineEdit(QLineEdit):
         self.julia_subprocess = None
         self.ui = None
         self._project = None
+        self.history = None
+        self.history_point = None
+        self.history_file_handle = None
 
     def setup_command_line(self, ui, project):
         self.ui = ui
         self._project = project
         self.julia_subprocess = self._project.get_julia_subprocess()
+        self.julia_subprocess.start_if_not_running()
+        self.history = list()
+        self.history_point = 0
+        julia_history_file = os.path.join(Path.home(), ".spine_toolbox_julia_history")
+        self.history_file_handle = open(julia_history_file, 'a+')
+        with open(julia_history_file, 'r') as f:
+            for line in f:
+                self.history.append(line.strip())
 
     def keyPressEvent(self, e):
         """Hide this widget when escape key is pressed.
@@ -62,8 +74,32 @@ class CustomQLineEdit(QLineEdit):
             julia_prompt = "<span style='color:green;white-space: pre;'><b>julia> </b></span>"
             self.ui.add_process_message(julia_prompt + self.text())
             command = self.text() + "\n"
-            ret = self.julia_subprocess.write_on_process(command)
-            if ret:
+            if self.julia_subprocess.write_on_process(command):
+                self.history_point = 0
+                # save command only if history is empty or command is different from last one 
+                if not self.history or self.text() != self.history[-1]:
+                    self.history.append(self.text())
+                    self.history_file_handle.write(command)
+                    self.history_file_handle.flush()
                 self.setText("")
+        elif e.key() == Qt.Key_Up:
+            try:
+                self.history[self.history_point-1]
+                self.history_point -= 1
+                self.update_text_from_history()
+            except IndexError:
+                pass
+        elif e.key() == Qt.Key_Down:
+            self.history_point += 1
+            if self.history_point > 0:
+                self.history_point = 0
+            self.update_text_from_history()
         else:
             super().keyPressEvent(e)
+
+
+    def update_text_from_history(self):
+        if self.history_point:
+            self.setText(self.history[self.history_point])
+        else:
+            self.setText("")
