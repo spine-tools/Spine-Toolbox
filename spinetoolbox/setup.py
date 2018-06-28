79aa30c5769600fa30e6abde73bae52ce112808e
Pekka Savolainen <pekka.t.savolainen@vtt.fi>
2018-06-28 15:43:09 +0300

    Updated setup.py to fix problems with .msi installer described in Issue #88

--- spinetoolbox/setup.py
+++ spinetoolbox/setup.py
@@ -20,10 +20,10 @@
 """
 CX-FREEZE setup file for Spine Toolbox.
 
-    - Create a Windows Installer distribution package (.msi) with the following command:
-        python setup.py bdist_msi
     - Build the application into /build directory with the following command:
         python setup.py build
+    - Create a Windows Installer distribution package (.msi) with the following command:
+        python setup.py bdist_msi
 
 :author: Pekka Savolainen <pekka.t.savolainen@vtt.fi>
 :date:   29.5.2018
@@ -40,41 +40,35 @@ def main(argv):
     python_dir = os.path.dirname(sys.executable)
     os.environ['TCL_LIBRARY'] = os.path.join(python_dir, 'tcl', 'tcl8.6')
     os.environ['TK_LIBRARY'] = os.path.join(python_dir, 'tcl', 'tk8.6')
-    # qt.conf (the one that references PySide2 that is included with the installed application)
-    qt_conf = os.path.join(APPLICATION_PATH, "build", "qt.conf")
     # Path to built documentation (No need for sources)
     doc_path = os.path.abspath(os.path.join(APPLICATION_PATH, os.path.pardir, "docs", "build"))
     # Set Windows .msi installer default install path to C:\SpineToolbox-version
     systemdrive = os.environ['SYSTEMDRIVE']
     default_install_dir = os.path.join(systemdrive, os.path.sep, "SpineToolbox-" + SPINE_TOOLBOX_VERSION)
-
+    # Hardcoded path to msvcr120.dll because include_msvcr option does not seem to do anything
+    msvcr120_dll = os.path.join(systemdrive, os.path.sep, "Windows", "System32", "msvcr120.dll")
+    if not os.path.isfile(msvcr120_dll):
+        print("\nmsvcr120.dll not found in path:{0}".format(msvcr120_dll))
+        return
     # Most dependencies are automatically detected, but it might need fine tuning.
-    buildOptions = dict(packages=[],
-                        excludes=["tkinter"],
-                        includes=["atexit", "idna.idnadata", "pygments.lexers.python", "pygments.lexers.shell",
-                                  "pygments.lexers.julia", "qtconsole.client", "sqlalchemy.sql.default_comparator"],
-                        include_files=[(doc_path, "docs/")])
-    # buildOptions = dict(packages=[],
-    #                     excludes=["tkinter"],
-    #                     includes=["atexit", "idna.idnadata", "pygments.lexers.python", "pygments.lexers.shell",
-    #                               "pygments.lexers.julia", "qtconsole.client", "sqlalchemy.sql.default_comparator"],
-    #                     include_files=[qt_conf] + [(doc_path, "docs/")])
-
+    build_exe_options = {"packages": [],
+                             "excludes": ["tkinter"],
+                             "includes": ["atexit", "idna.idnadata", "pygments.lexers.python",
+                                          "pygments.lexers.shell", "pygments.lexers.julia",
+                                          "qtconsole.client", "sqlalchemy.sql.default_comparator"],
+                             "include_files": [(doc_path, "docs/"), msvcr120_dll],
+                             "include_msvcr": True}
+    bdist_msi_options = {"initial_target_dir": default_install_dir}
     # This does not show logging messages
     # base = "Win32GUI" if sys.platform == "win32" else None
-
     # This opens a console that shows also logging messages
     base = "Console" if sys.platform == "win32" else None
-
     executables = [Executable("spinetoolbox.py", base=base)]
-
-    bdist_msi_options = dict(initial_target_dir=default_install_dir)
-
     setup(name="Spine Toolbox",
           version=SPINE_TOOLBOX_VERSION,
           description="An application to define, manage, and execute various energy system simulation models.",
           author="Spine project",
-          options=dict(build_exe=buildOptions, bdist_msi=bdist_msi_options),
+          options={"build_exe": build_exe_options, "bdist_msi": bdist_msi_options},
           executables=executables)
 
 
