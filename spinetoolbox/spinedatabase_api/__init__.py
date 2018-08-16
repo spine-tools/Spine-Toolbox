7a24bba92abfd31f0550a8aefa1bebc2a3b449da
Manuel Marin <manuelma@kth.se>
2018-08-16 23:00:13 +0200

    Add spinedatabase_api.

---
+++ spinetoolbox/spinedatabase_api/__init__.py
@@ -0,0 +1,5 @@
+from .database_mapping import DatabaseMapping
+from .exception import SpineDBAPIError, TableNotFoundError, RecordNotFoundError, ParameterValueError
+from .helpers import create_new_spine_database, copy_database
+
+name = "spinedatabase_api"
