cd2cc4c7792539219b098208cc042348813cdad3
Pekka Savolainen <pekka.t.savolainen@vtt.fi>
2018-08-24 12:49:54 +0300

    Reorganizing documentation. Autogenerated documentation source files are now in their own directory.

--- bin/build_doc.bat
+++ bin/build_doc.bat
@@ -2,6 +2,6 @@
 @TITLE Build Spine Toolbox docs
 
 ECHO Building Spine Toolbox documentation
-sphinx-apidoc -f -o ../docs/source/ ../spinetoolbox/
+sphinx-apidoc -f -o ../docs/source/apidocs ../spinetoolbox/
 cd ../docs
 make html
