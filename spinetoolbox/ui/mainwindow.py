b1573fd2daeaad92b6fb230f139d17ff169670c9
Pekka Savolainen <pekka.t.savolainen@vtt.fi>
2018-10-24 19:22:35 +0300

    Simplified installation instructions in Readme.md and updated requirements files.

--- spinetoolbox/ui/mainwindow.py
+++ spinetoolbox/ui/mainwindow.py
@@ -1083,7 +1083,7 @@ class Ui_MainWindow(object):
         self.actionRestore_Dock_Widgets.setToolTip(QtWidgets.QApplication.translate("MainWindow", "<html><head/><body><p>Dock all floating and/or hidden dock widgets back to main window.</p></body></html>", None, -1))
 
 from widgets.custom_qtextbrowser import CustomQTextBrowser
-from widgets.custom_qtreeview import ReferencesTreeView, DataTreeView
+from widgets.custom_qtreeview import DataTreeView, ReferencesTreeView
 from widgets.custom_qlineedit import CustomQLineEdit
 from widgets.custom_qgraphicsview import CustomQGraphicsView
 import resources_icons_rc
