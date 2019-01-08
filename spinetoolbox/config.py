dc56f6d9c6936d8fa2ecac2b6cc200fcfe42fffe
Pekka Savolainen <pekka.t.savolainen@vtt.fi>
2019-01-08 17:16:10 +0200

    Updated required spinedatabase_api version.

--- spinetoolbox/config.py
+++ spinetoolbox/config.py
@@ -21,7 +21,7 @@ import os
 from PySide2.QtGui import QColor
 
 SPINE_TOOLBOX_VERSION = "0.2alpha"
-REQUIRED_SPINE_DBAPI_VERSION = "0.0.12"
+REQUIRED_SPINE_DBAPI_VERSION = "0.0.13"
 ERROR_COLOR = QColor('red')
 SUCCESS_COLOR = QColor('green')
 NEUTRAL_COLOR = QColor('blue')
