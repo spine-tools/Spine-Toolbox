11ef638d75b864d125978327be5e80c89a15c481
Manuel Marin <manuelma@kth.se>
2019-03-26 09:47:54 -0400

    Issue data#22, finish renaming stuff

--- bin/upgrade_spinedb_api.bat
+++ bin/upgrade_spinedb_api.bat
@@ -1,15 +1,15 @@
 @ECHO OFF
-@TITLE Upgrading spinedatabase_api
+@TITLE Upgrading spinedb_api
 
-ECHO This is a script for upgrading spinedatabase_api.
+ECHO This is a script for upgrading spinedb_api.
 ECHO Copyright (C) ^<2017-2018^>  ^<Spine project consortium^>
 ECHO This program comes with ABSOLUTELY NO WARRANTY; This is free software, and you are welcome to redistribute it
 ECHO under certain conditions; See files COPYING and COPYING.LESSER for details.
 ECHO.
 
 ECHO USAGE:
-ECHO 'upgrade_spinedatabase_api.bat'            upgrade from master branch
-ECHO 'upgrade_spinedatabase_api.bat dev'        upgrade from dev branch
+ECHO 'upgrade_spinedb_api.bat'            upgrade from master branch
+ECHO 'upgrade_spinedb_api.bat dev'        upgrade from dev branch
 ECHO.
 
 if "%1"=="" goto :update_master
