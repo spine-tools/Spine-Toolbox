179cff6196f122f1d85ede4f14dd66e9d24c6b90
Erkka Rinne <erkka.rinne@vtt.fi>
2019-08-06 12:48:45 +0300

    Fixed *bin/build_docs.sh* script
    Output dir of `sphinx-apidoc` was wrong.

--- bin/build_doc.sh
+++ bin/build_doc.sh
@@ -2,7 +2,7 @@
 #@TITLE Build Spine Toolbox docs
 
 echo Building Spine Toolbox documentation
-sphinx-apidoc -f -o ../docs/source/ ../spinetoolbox/ ../spinetoolbox/setup.py
+sphinx-apidoc -f -o ../docs/source/apidocs ../spinetoolbox/ ../spinetoolbox/setup.py
 pushd ../docs
 make html
 popd
