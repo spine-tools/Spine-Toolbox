9878b984cbdf13ab051402ce45d4ce30a773c93b
Manuel Marin <manuelma@kth.se>
2019-09-26 16:57:40 -0400

    Add tests for opening the tree view
    These two tests check that entering a url and pressing open tree view 
    works as expected.
    
    Re issue #412

--- spinetoolbox/test/test_dataStore.py
+++ spinetoolbox/test/test_dataStore.py
@@ -26,6 +26,7 @@ from test.mock_helpers import MockQWidget, qsettings_value_side_effect
 from PySide2.QtWidgets import QApplication, QWidget
 from ui_main import ToolboxUI
 from spinedb_api import create_new_spine_database
+from widgets.tree_view_widget import TreeViewForm
 
 
 # noinspection PyUnusedLocal
@@ -43,6 +44,22 @@ class TestDataStore(unittest.TestCase):
             format='%(asctime)s %(levelname)s: %(message)s',
             datefmt='%Y-%m-%d %H:%M:%S',
         )
+
+        # Let's create a real db to more easily test complicated stuff (such as opening a tree view)
+        cls.file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "temp_db.sqlite")
+        if not os.path.exists(cls.file_path):
+            with open(cls.file_path, 'w'):
+                pass
+        cls.url = "sqlite:///" + cls.file_path
+        create_new_spine_database(cls.url)
+
+    @classmethod
+    def tearDownClass(cls):
+        """Remove the file path."""
+        try:
+            os.remove(cls.file_path)
+        except OSError:
+            pass
 
     def setUp(self):
         """Overridden method. Runs before each test. Makes instance of ToolboxUI class.
@@ -170,18 +187,11 @@ class TestDataStore(unittest.TestCase):
         self.assertTrue(os.path.exists(db_line_edit.text()))
         self.assertTrue(os.path.isfile(db_line_edit.text()))
 
-    def test_load_reference(self):
-        """Test that reference is loaded into selections on Data Store creation,
+    def test_load_url(self):
+        """Test that url is loaded into selections on Data Store creation,
         and then shown in the ui when Data Store is activated.
         """
-        # FIXME: For now it only tests sqlite references
-        file_path = os.path.join(self.toolbox.project().project_dir, "mock_db.sqlite")
-        if not os.path.exists(file_path):
-            with open(file_path, 'w'):
-                pass
-        url = "sqlite:///" + file_path
-        create_new_spine_database(url)
-        item = dict(name="DS", description="", url=url, x=0, y=0)
+        item = dict(name="DS", description="", url=self.url, x=0, y=0)
         self.toolbox.project().add_project_items("Data Stores", item)  # Create Data Store to project
         ind = self.toolbox.project_item_model.find_item("DS")
         data_store = self.toolbox.project_item_model.project_item(ind)  # Find item from project item model
@@ -244,6 +254,49 @@ class TestDataStore(unittest.TestCase):
         # noinspection PyArgumentList
         clipboard_text = QApplication.clipboard().text()
         self.assertEqual(clipboard_text, url)
+
+    def test_open_treeview1(self):
+        """Test that selecting the 'sqlite' dialect, browsing to an existing db file,
+        and pressing open tree view works as expected.
+        """
+        item = dict(name="DS", description="", x=0, y=0, url=None, reference=None)
+        self.toolbox.project().add_project_items("Data Stores", item)  # Create Data Store to project
+        ind = self.toolbox.project_item_model.find_item("DS")
+        data_store = self.toolbox.project_item_model.project_item(ind)  # Find item from project item model
+        data_store.activate()
+        self.assertIsNone(data_store.tree_view_form)
+        # Select the sqlite dialect
+        self.ds_properties_ui.comboBox_dialect.activated[str].emit("sqlite")
+        # Browse to an existing db file
+        with mock.patch("project_items.data_store.data_store.QFileDialog") as mock_qfile_dialog:
+            mock_qfile_dialog.getOpenFileName.side_effect = lambda *args: [self.file_path]
+            self.ds_properties_ui.toolButton_open_sqlite_file.click()
+        # Open treeview
+        self.ds_properties_ui.pushButton_ds_tree_view.click()
+        self.assertIsInstance(data_store.tree_view_form, TreeViewForm)
+        self.assertEqual(str(data_store.tree_view_form.db_maps[0].db_url), str(self.url))
+        data_store.tree_view_form.close()
+
+    def test_open_treeview2(self):
+        """Test that selecting the 'sqlite' dialect, typing the path to an existing db file,
+        and pressing open tree view works as expected.
+        """
+        item = dict(name="DS", description="", x=0, y=0, url=None, reference=None)
+        self.toolbox.project().add_project_items("Data Stores", item)  # Create Data Store to project
+        ind = self.toolbox.project_item_model.find_item("DS")
+        data_store = self.toolbox.project_item_model.project_item(ind)  # Find item from project item model
+        data_store.activate()
+        self.assertIsNone(data_store.tree_view_form)
+        # Select the sqlite dialect
+        self.ds_properties_ui.comboBox_dialect.activated[str].emit("sqlite")
+        # Type the path to an existing db file
+        self.ds_properties_ui.lineEdit_database.setText(self.file_path)
+        self.ds_properties_ui.lineEdit_database.editingFinished.emit()
+        # Open treeview
+        self.ds_properties_ui.pushButton_ds_tree_view.click()
+        self.assertIsInstance(data_store.tree_view_form, TreeViewForm)
+        self.assertEqual(str(data_store.tree_view_form.db_maps[0].db_url), str(self.url))
+        data_store.tree_view_form.close()
 
 
 if __name__ == '__main__':
