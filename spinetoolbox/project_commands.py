354becd0119ce70af601d52b75b13e21be3a69d4
Manuel Marin <manuelma@kth.se>
2020-02-20 11:48:36 -0400

    Fix small bug in setting text for the moving items command
    Re #39

--- spinetoolbox/project_commands.py
+++ spinetoolbox/project_commands.py
@@ -238,7 +238,7 @@ class MoveIconCommand(QUndoCommand):
         self.previous_pos = {x: x._previous_pos for x in graphics_item.selected_icons}
         self.current_pos = {x: x._current_pos for x in graphics_item.selected_icons}
         if len(graphics_item.selected_icons) == 1:
-            self.setText(f"move {graphics_item.selected_icons[0]._project_item.name}")
+            self.setText(f"move {list(graphics_item.selected_icons)[0]._project_item.name}")
         else:
             self.setText("move multiple items")
 
