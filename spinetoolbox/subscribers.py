d872e4943aca6a151303a760df089b8c9ffa8229
RichieBrady <richard.brady@ucdconnect.ie>
2020-09-24 12:07:35 +0100

    Adding copyright header and moved author docstring to before imports

--- spinetoolbox/subscribers.py
+++ spinetoolbox/subscribers.py
@@ -1,4 +1,13 @@
-from PySide2.QtCore import QObject, Signal
+######################################################################################################################
+# Copyright (C) 2017-2020 Spine project consortium
+# This file is part of Spine Toolbox.
+# Spine Toolbox is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General
+# Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
+# any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+# Public License for more details. You should have received a copy of the GNU Lesser General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
+######################################################################################################################
 
 """
 Module contains subscriber classes that subscribe to the spine engine EventPublisher.
@@ -7,6 +16,8 @@ Each subscriber is created to handle a specific event in the engine.
 :author: R. Brady (UCD)
 :date:   21.9.2020
 """
+
+from PySide2.QtCore import QObject, Signal
 
 
 class NodeExecStartedSubscriber(QObject):
@@ -60,6 +71,7 @@ class LoggingSubscriber(QObject):
      A subscriber class for the log_event event which is a named event in the EventPublisher class of spine_engine.py
      A simple class to test logging a message originating from SpineEngine to a toolbox widget.
     """
+
     def __init__(self, logger):
         """
         Args:
--- tests/test_subscribers.py
+++ tests/test_subscribers.py
@@ -1,9 +1,13 @@
-import unittest
-
-from PySide2.QtCore import Slot
-from spine_engine import ExecutionDirection, SpineEngineState
-
-from spinetoolbox.subscribers import NodeExecFinishedSubscriber, NodeExecStartedSubscriber, LoggingSubscriber
+######################################################################################################################
+# Copyright (C) 2017-2020 Spine project consortium
+# This file is part of Spine Toolbox.
+# Spine Toolbox is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General
+# Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
+# any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+# Public License for more details. You should have received a copy of the GNU Lesser General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
+######################################################################################################################
 
 """
 Unit tests for Subscriber classes.
@@ -11,6 +15,13 @@ Unit tests for Subscriber classes.
 :author: R. Brady (UCD)
 :date:   21.9.2020
 """
+
+import unittest
+
+from PySide2.QtCore import Slot
+from spine_engine import ExecutionDirection, SpineEngineState
+
+from spinetoolbox.subscribers import NodeExecFinishedSubscriber, NodeExecStartedSubscriber, LoggingSubscriber
 
 
 class TestSubscribers(unittest.TestCase):
