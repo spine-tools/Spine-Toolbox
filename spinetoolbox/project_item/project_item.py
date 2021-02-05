bac504ca649b96dd4d71c77263919f6ee9915541
Pekka Savolainen <pekka.t.savolainen@vtt.fi>
2021-02-05 12:45:11 +0200

    Add missing @Slot and get fix for broken Tool properties open data dir button from spine_items

--- spinetoolbox/project_item/project_item.py
+++ spinetoolbox/project_item/project_item.py
@@ -413,7 +413,8 @@ class ProjectItem(MetaObject):
             self.update_name_label()
         self.get_icon().update_name_item(new_name)
 
-    def open_directory(self):
+    @Slot(bool)
+    def open_directory(self, checked=False):
         """Open this item's data directory in file explorer."""
         url = "file:///" + self.data_dir
         # noinspection PyTypeChecker, PyCallByClass, PyArgumentList
