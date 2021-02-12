f316d483b90557d78cec73ed4efc44f420ea7da8
Pekka Savolainen <pekka.t.savolainen@vtt.fi>
2021-02-12 16:00:26 +0200

    Removed two QTreeView classes and get them from spine-items instead.
    Closes #1216

--- spinetoolbox/widgets/custom_qtreeview.py
+++ spinetoolbox/widgets/custom_qtreeview.py
@@ -23,8 +23,7 @@ from PySide2.QtGui import QDrag
 
 
 class CopyTreeView(QTreeView):
-    """Custom QTreeView class with copy support.
-    """
+    """Custom QTreeView class with copy support."""
 
     def __init__(self, parent):
         """Initialize the view."""
@@ -43,129 +42,6 @@ class CopyTreeView(QTreeView):
         content = "\n".join(values)
         QApplication.clipboard().setText(content)
         return True
-
-
-class ReferencesTreeView(QTreeView):
-    """Custom QTreeView class for 'References' in Data Connection properties.
-
-    Attributes:
-        parent (QWidget): The parent of this view
-    """
-
-    files_dropped = Signal("QVariant", name="files_dropped")
-    del_key_pressed = Signal(name="del_key_pressed")
-
-    def __init__(self, parent):
-        """Initialize the view."""
-        super().__init__(parent=parent)
-
-    def dragEnterEvent(self, event):
-        """Accept file drops from the filesystem."""
-        urls = event.mimeData().urls()
-        for url in urls:
-            if not url.isLocalFile():
-                event.ignore()
-                return
-            if not os.path.isfile(url.toLocalFile()):
-                event.ignore()
-                return
-        event.accept()
-        event.setDropAction(Qt.LinkAction)
-
-    def dragMoveEvent(self, event):
-        """Accept event."""
-        event.accept()
-
-    def dropEvent(self, event):
-        """Emit files_dropped signal with a list of files for each dropped url."""
-        self.files_dropped.emit([url.toLocalFile() for url in event.mimeData().urls()])
-
-    def keyPressEvent(self, event):
-        """Overridden method to make the view support deleting items with a delete key."""
-        super().keyPressEvent(event)
-        if event.key() == Qt.Key_Delete:
-            self.del_key_pressed.emit()
-
-
-class DataTreeView(QTreeView):
-    """Custom QTreeView class for 'Data' in Data Connection properties.
-
-    Attributes:
-        parent (QWidget): The parent of this view
-    """
-
-    files_dropped = Signal("QVariant", name="files_dropped")
-    del_key_pressed = Signal(name="del_key_pressed")
-
-    def __init__(self, parent):
-        """Initialize the view."""
-        super().__init__(parent=parent)
-        self.drag_start_pos = None
-        self.drag_indexes = list()
-
-    def dragEnterEvent(self, event):
-        """Accept file drops from the filesystem."""
-        urls = event.mimeData().urls()
-        for url in urls:
-            if not url.isLocalFile():
-                event.ignore()
-                return
-            if not os.path.isfile(url.toLocalFile()):
-                event.ignore()
-                return
-        event.accept()
-        event.setDropAction(Qt.CopyAction)
-
-    def dragMoveEvent(self, event):
-        """Accept event."""
-        event.accept()
-
-    def dropEvent(self, event):
-        """Emit files_dropped signal with a list of files for each dropped url."""
-        self.files_dropped.emit([url.toLocalFile() for url in event.mimeData().urls()])
-
-    def mousePressEvent(self, event):
-        """Register drag start position."""
-        if event.button() == Qt.LeftButton:
-            self.drag_start_pos = event.pos()
-            self.drag_indexes = self.selectedIndexes()
-        super().mousePressEvent(event)
-
-    def mouseMoveEvent(self, event):
-        """Start dragging action if needed."""
-        if not event.buttons() & Qt.LeftButton:
-            return
-        if not self.drag_start_pos:
-            return
-        if not self.drag_indexes:
-            return
-        if (event.pos() - self.drag_start_pos).manhattanLength() < QApplication.startDragDistance():
-            return
-        drag = QDrag(self)
-        mimeData = QMimeData()
-        urls = list()
-        for index in self.drag_indexes:
-            file_path = index.data(Qt.UserRole)
-            urls.append(QUrl.fromLocalFile(file_path))
-        mimeData.setUrls(urls)
-        drag.setMimeData(mimeData)
-        icon = self.drag_indexes[0].data(Qt.DecorationRole)
-        if icon:
-            pixmap = icon.pixmap(32, 32)
-            drag.setPixmap(pixmap)
-            drag.setHotSpot(pixmap.rect().center())
-        drag.exec_()
-
-    def mouseReleaseEvent(self, event):
-        """Forget drag start position"""
-        self.drag_start_pos = None
-        super().mouseReleaseEvent(event)  # Fixes bug in extended selection
-
-    def keyPressEvent(self, event):
-        """Overridden method to make the view support deleting items with a delete key."""
-        super().keyPressEvent(event)
-        if event.key() == Qt.Key_Delete:
-            self.del_key_pressed.emit()
 
 
 class SourcesTreeView(QTreeView):
