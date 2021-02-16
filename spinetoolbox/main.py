31b745ff1ef4c241def4d4bcf4a15fcbbec81a7c
Manuel Marin <manuelma@kth.se>
2021-02-16 13:57:04 +0100

    Try fix for `could not load the qt platform plugin`
    Re #1227
    
    This seems to help. Setting the QT_QPA_PLATFORM_PLUGIN_PATH
    environment variable to the path of PySide's platforms
    folder, before doing anything else.

--- spinetoolbox/main.py
+++ spinetoolbox/main.py
@@ -15,7 +15,14 @@ Provides the main() function.
 :author: A. Soininen (VTT)
 :date:   4.10.2019
 """
+import os
+import PySide2
 
+dirname = os.path.dirname(PySide2.__file__)
+plugin_path = os.path.join(dirname, "plugins", "platforms")
+os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"] = plugin_path
+
+# pylint: disable=wrong-import-position, wrong-import-order
 from argparse import ArgumentParser
 import sys
 import logging
