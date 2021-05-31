8a26fdce73e1ec77f767dbed940fc4ae5d430a24
Manuel Marin <manuelma@kth.se>
2021-05-31 14:42:28 +0200

    Improve error message in spine DB editor

--- spinetoolbox/spine_db_editor/mvcmodels/parameter_mixins.py
+++ spinetoolbox/spine_db_editor/mvcmodels/parameter_mixins.py
@@ -561,13 +561,14 @@ class ValidateValueInListMixin(ConvertToDBMixin):
         value = item.get("value")
         if value is None:
             return item, err
+        str_value = str(value, "UTF8")
         param_def_id = self._get_parameter_definition_id(db_map, item)
         param_def = self.db_mngr.get_item(db_map, "parameter_definition", param_def_id)
         value_list = self.db_mngr.get_parameter_value_list(db_map, param_def.get("value_list_id"), role=Qt.EditRole)
-        if value_list and str(value, "UTF8") not in value_list:
+        if value_list and str_value not in value_list:
             item["has_valid_value_from_list"] = False
             msg = (
-                f"Invalid value '{value}' for parameter '{param_def['parameter_name']}', "
+                f"Invalid value '{str_value}' for parameter '{param_def['parameter_name']}', "
                 f"valid values are {', '.join(value_list)}"
             )
             return item, err + [msg]
