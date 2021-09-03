b3e167a6180c86df9f547317593fd97b2afaa46b
Pekka T Savolainen <pekka.t.savolainen@vtt.fi>
2021-09-03 12:02:09 +0300

    Bump version to 0.6.3

--- spinetoolbox/version.py
+++ spinetoolbox/version.py
@@ -42,8 +42,8 @@ class VersionInfo(NamedTuple):
 
 major = 0
 minor = 6
-micro = 2
-releaselevel = "dev"
+micro = 3
+releaselevel = "final"
 serial = 0
 __version_info__ = VersionInfo(major, minor, micro, releaselevel, serial)
 __version__ = str(__version_info__)
