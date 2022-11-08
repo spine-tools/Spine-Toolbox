7c5413510b0495d61158abdd2cf41b3411fcbb68
Antti Soininen <antti.soininen@vtt.fi>
2022-11-08 15:33:16 +0200

    Bump version to 0.6.15

--- spinetoolbox/version.py
+++ spinetoolbox/version.py
@@ -42,7 +42,7 @@ class VersionInfo(NamedTuple):
 
 major = 0
 minor = 6
-micro = 14
+micro = 15
 releaselevel = "dev"
 serial = 0
 __version_info__ = VersionInfo(major, minor, micro, releaselevel, serial)
