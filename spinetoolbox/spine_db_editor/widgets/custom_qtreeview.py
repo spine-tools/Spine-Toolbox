f392951286409c3ee089f4adbd87c0649080a253
Henrik Koski <98282892+PiispaH@users.noreply.github.com>
2023-06-01 10:41:32 +0300

    Issue 2134 db editor filter behavior (#2139)
    * db editors filter
    
    The filter has now been updated between Alternative tree, Scenario tree and Object tree. The user can select multiple elements across these three trees by holding down ctrl. Selecting with ctrl+shift also works across different trees.
    
    Re #2134
    
    ---------
    
    Co-authored-by: Henrik Koski <henrik.koski@vtt.fi>

--- spinetoolbox/spine_db_editor/widgets/custom_qtreeview.py
+++ spinetoolbox/spine_db_editor/widgets/custom_qtreeview.py
@@ -15,7 +15,7 @@ Classes for custom QTreeView.
 import pickle
 
 from PySide6.QtWidgets import QApplication, QMenu, QAbstractItemView
-from PySide6.QtCore import Signal, Slot, Qt, QEvent, QTimer, QModelIndex, QItemSelection
+from PySide6.QtCore import Signal, Slot, Qt, QEvent, QTimer, QModelIndex, QItemSelection, QSignalBlocker
 from PySide6.QtGui import QMouseEvent, QIcon
 
 from spinetoolbox.widgets.custom_qtreeview import CopyPasteTreeView
@@ -150,8 +150,6 @@ class EntityTreeView(ResizableTreeView):
         """Classifies selection by item type and emits signal."""
         self._spine_db_editor.refresh_copy_paste_actions()
         self._refresh_selected_indexes()
-        if not self.selectionModel().hasSelection():
-            return
         self.tree_selection_changed.emit(self._selected_indexes)
 
     def _refresh_selected_indexes(self):
@@ -168,7 +166,8 @@ class EntityTreeView(ResizableTreeView):
         """Clears the selection if any."""
         selection_model = self.selectionModel()
         if selection_model.hasSelection():
-            selection_model.clearSelection()
+            with QSignalBlocker(selection_model) as _:
+                selection_model.clearSelection()
 
     @busy_effect
     def fully_expand(self):
@@ -671,7 +670,7 @@ class AlternativeTreeView(ItemTreeView):
 class ScenarioTreeView(ItemTreeView):
     """Custom QTreeView for the scenario tree in SpineDBEditor."""
 
-    alternative_selection_changed = Signal(dict)
+    scenario_selection_changed = Signal(dict)
 
     def __init__(self, parent):
         """
@@ -729,7 +728,7 @@ class ScenarioTreeView(ItemTreeView):
 
     @Slot(QItemSelection, QItemSelection)
     def _handle_selection_changed(self, selected, deselected):
-        """Emits alternative_selection_changed with the current selection."""
+        """Emits scenario_selection_changed with the current selection."""
         self._selected_alternative_ids.clear()
         for index in self.selectionModel().selectedRows(column=0):
             item = self.model().item_from_index(index)
@@ -737,7 +736,7 @@ class ScenarioTreeView(ItemTreeView):
                 self._selected_alternative_ids.setdefault(item.db_map, set()).update(item.alternative_id_list)
             elif isinstance(item, ScenarioAlternativeItem) and item.alternative_id is not None:
                 self._selected_alternative_ids.setdefault(item.db_map, set()).add(item.alternative_id)
-        self.alternative_selection_changed.emit(self._selected_alternative_ids)
+        self.scenario_selection_changed.emit(self._selected_alternative_ids)
 
     def remove_selected(self):
         """See base class."""
