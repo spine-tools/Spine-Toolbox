aa4d54b47bb85909d324aad291aa55a899b64441
Antti Soininen <antti.soininen@vtt.fi>
2023-09-06 16:28:45 +0300

    Fix execution tests
    Tests were broken due to changes in spinedb_api
    
    Re #2294

--- execution_tests/scenario_filters/tool.py
+++ execution_tests/scenario_filters/tool.py
@@ -2,8 +2,7 @@ import sys
 from spinedb_api import DatabaseMapping, from_database
 
 url = sys.argv[1]
-db_map = DatabaseMapping(url)
-parameter_value = from_database(db_map.query(db_map.parameter_value_sq).first().value)
+with DatabaseMapping(url) as db_map:
+    parameter_value = from_database(db_map.query(db_map.parameter_value_sq).first().value)
 with open("out.dat", "w") as out_file:
     out_file.write(f"{parameter_value}")
-db_map.connection.close()
