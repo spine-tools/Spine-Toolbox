eec0a47b0fee3f226a316dfb48888573943299e6
Manuel <chacarero@gmail.com>
2023-09-17 15:17:27 +0200

    Fix typo

--- spinetoolbox/spine_db_editor/widgets/custom_qgraphicsviews.py
+++ spinetoolbox/spine_db_editor/widgets/custom_qgraphicsviews.py
@@ -667,7 +667,7 @@ class EntityQGraphicsView(CustomQGraphicsView):
     @Slot(bool)
     def export_as_video(self):
         try:
-            import cv2xx
+            import cv2
         except ModuleNotFoundError:
             self._spine_db_editor.msg_error.emit(
                 "Export as video requires <a href='https://pypi.org/project/opencv-python/'>opencv-python</a>"
