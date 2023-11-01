930ae2f3e333b89c7f9cc0c8f5ca772814a53f39
Antti Soininen <antti.soininen@vtt.fi>
2023-11-01 13:03:30 +0200

    Set worker's fetch parents obsolete on database close
    This prevents applying pending changes after the database is gone.
    Fixes a Traceback in tests.

--- spinetoolbox/spine_db_worker.py
+++ spinetoolbox/spine_db_worker.py
@@ -184,6 +184,10 @@ class SpineDBWorker(QObject):
 
     def close_db_map(self):
         self._do_fetch_more = lambda worker, *args, **kwargs: None
+        for parents in self._parents_by_type.values():
+            for parent in parents:
+                if not parent.is_obsolete:
+                    parent.set_obsolete(True)
         self._db_map.close()
 
     @busy_effect
