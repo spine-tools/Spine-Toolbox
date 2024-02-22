1272bf3bf209c9c66ce88cbaf9237529486bfcde
Antti Soininen <antti.soininen@vtt.fi>
2024-02-22 14:06:05 +0200

    Add GitHub action to build Toolbox bundle

--- PyInstaller hooks/hook-tabulator.py
+++ PyInstaller hooks/hook-tabulator.py
@@ -1,4 +1,5 @@
 from PyInstaller.utils.hooks import collect_data_files
 
 datas = collect_data_files("tabulator")
-datas += collect_data_files("tabulator", subdir="loader", include_py_files=True)
+datas += collect_data_files("tabulator", subdir="loaders", include_py_files=True)
+datas += collect_data_files("tabulator", subdir="parsers", include_py_files=True)
