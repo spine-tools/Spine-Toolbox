d954d8b90fe64f6e56e624bdcdd44ad89b8bf03f
Henrik Koski <henrik.koski@vtt.fi>
2024-06-12 12:54:02 +0300

    Revert "Order tables by class dimension (#2827)"
    This reverts commit 71da77ca60af87c1896292205a1b08e14a760906.

--- spinetoolbox/spine_db_editor/mvcmodels/single_models.py
+++ spinetoolbox/spine_db_editor/mvcmodels/single_models.py
@@ -12,7 +12,7 @@
 
 """Single models for parameter definitions and values (as 'for a single entity')."""
 from PySide6.QtCore import Qt
-from spinetoolbox.helpers import DB_ITEM_SEPARATOR, plain_to_rich, order_key
+from spinetoolbox.helpers import DB_ITEM_SEPARATOR, plain_to_rich
 from ...mvcmodels.minimal_table_model import MinimalTableModel
 from ..mvcmodels.single_and_empty_model_mixins import SplitValueAndTypeMixin, MakeEntityOnTheFlyMixin
 from ...mvcmodels.shared import PARSED_ROLE, DB_MAP_ROLE
@@ -62,12 +62,7 @@ class SingleModelBase(HalfSortedTableModel):
     def __lt__(self, other):
         if self.entity_class_name == other.entity_class_name:
             return self.db_map.codename < other.db_map.codename
-        keys = []
-        for model in (self, other):
-            dim = model.dimension_id_list
-            class_name = model.entity_class_name
-            keys.append((dim, class_name))
-        return keys[0] < keys[1]
+        return self.entity_class_name < other.entity_class_name
 
     @property
     def item_type(self):
@@ -370,7 +365,7 @@ class SingleParameterDefinitionModel(SplitValueAndTypeMixin, ParameterMixin, Sin
 
     def _sort_key(self, element):
         item = self.db_item_from_id(element)
-        return order_key(item.get("name", ""))
+        return item.get("name", "")
 
     def _do_update_items_in_db(self, db_map_data):
         self.db_mngr.update_parameter_definitions(db_map_data)
@@ -392,10 +387,7 @@ class SingleParameterValueModel(
 
     def _sort_key(self, element):
         item = self.db_item_from_id(element)
-        byname = order_key("".join(item.get("entity_byname", ())))
-        param_name = order_key(item.get("parameter_name", ""))
-        alt_name = order_key(item.get("alternative_name", ""))
-        return byname, param_name, alt_name
+        return (item.get("entity_byname", ()), item.get("parameter_name", ""), item.get("alternative_name", ""))
 
     def _do_update_items_in_db(self, db_map_data):
         self.db_mngr.update_parameter_values(db_map_data)
@@ -410,9 +402,7 @@ class SingleEntityAlternativeModel(MakeEntityOnTheFlyMixin, EntityMixin, FilterE
 
     def _sort_key(self, element):
         item = self.db_item_from_id(element)
-        byname = order_key("".join(item.get("entity_byname", ())))
-        alt_name = order_key("".join(item.get("alternative_name", "")))
-        return byname, alt_name
+        return (item.get("entity_byname", ()), item.get("alternative_name", ""))
 
     @property
     def _references(self):
