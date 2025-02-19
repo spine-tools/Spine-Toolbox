a58f1043dbef2349970a187ffb999127732afe38
Antti Soininen <antti.soininen@vtt.fi>
2025-02-19 16:19:55 +0200

    Improve Importer documentation in User guide
    Accidentally rewrote all .ui files. Just in case, I guess.
    
    Re #2836

--- spinetoolbox/ui/link_properties.py
+++ spinetoolbox/ui/link_properties.py
@@ -14,7 +14,7 @@
 ################################################################################
 ## Form generated from reading UI file 'link_properties.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.7.3
+## Created by: Qt User Interface Compiler version 6.8.2
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
