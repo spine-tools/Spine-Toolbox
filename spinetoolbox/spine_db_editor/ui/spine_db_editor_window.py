d3fd587199a99cc4dceaa84a86bc518d8e615df1
jkiviluo <juha.kiviluoma@vtt.fi>
2025-03-13 22:34:25 +0200

    Added shortcuts to ui files

--- spinetoolbox/spine_db_editor/ui/spine_db_editor_window.py
+++ spinetoolbox/spine_db_editor/ui/spine_db_editor_window.py
@@ -14,7 +14,7 @@
 ################################################################################
 ## Form generated from reading UI file 'spine_db_editor_window.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.8.2
+## Created by: Qt User Interface Compiler version 6.7.2
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
