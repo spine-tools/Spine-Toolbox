8a9fb06e241ba26a4282169a4802f6fcdeb516e9
Antti Soininen <antti.soininen@vtt.fi>
2025-05-14 10:51:19 +0300

    Use MagicMock instead of Mock in a test
    The test fails on Python 3.11 Linux for some strange reason.
    Hope this helps.

--- tests/spine_db_editor/widgets/test_SpineDBEditorWithDBMapping.py
+++ tests/spine_db_editor/widgets/test_SpineDBEditorWithDBMapping.py
@@ -30,7 +30,7 @@ class TestSpineDBEditorWithDBMapping(TestCaseWithQApplication):
             mock.patch("spinetoolbox.spine_db_editor.widgets.spine_db_editor.SpineDBEditor.restore_ui"),
             mock.patch("spinetoolbox.spine_db_editor.widgets.spine_db_editor.SpineDBEditor.show"),
         ):
-            mock_settings = mock.Mock()
+            mock_settings = mock.MagicMock()
             mock_settings.value.side_effect = lambda *args, **kwards: 0
             self.db_mngr = TestSpineDBManager(mock_settings, None)
             logger = mock.MagicMock()
