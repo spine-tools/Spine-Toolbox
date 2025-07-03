d72edcf9aae94e7ea9be04aa99adc9c5460085e2
Antti Soininen <antti.soininen@vtt.fi>
2025-07-03 15:25:53 +0300

    Fix 0-dimensional entity byname editors in DB editor
    Entity bynames are tuples nowadays - the editor needs to work with
    those, not strings.

--- tests/spine_db_editor/widgets/test_custom_delegates.py
+++ tests/spine_db_editor/widgets/test_custom_delegates.py
@@ -14,9 +14,11 @@
 import unittest
 from unittest import mock
 from PySide6.QtGui import QStandardItem, QStandardItemModel
+from PySide6.QtWidgets import QStyleOptionViewItem
 from spinetoolbox.helpers import signal_waiter
-from spinetoolbox.spine_db_editor.widgets.custom_delegates import BooleanValueDelegate
-from tests.mock_helpers import TestCaseWithQApplication
+from spinetoolbox.spine_db_editor.widgets.custom_delegates import BooleanValueDelegate, EntityBynameDelegate
+from spinetoolbox.spine_db_editor.widgets.custom_editors import GroupEditor
+from tests.mock_helpers import TestCaseWithQApplication, assert_table_model_data_pytest
 
 
 class TestBooleanValueDelegate(TestCaseWithQApplication):
@@ -55,5 +57,77 @@ class TestBooleanValueDelegate(TestCaseWithQApplication):
             data_committed_signal.emit.assert_not_called()
 
 
-if __name__ == "__main__":
-    unittest.main()
+class TestEntityBynameDelegate:
+    def test_create_editor_for_undefined_entity_with_empty_model(self, db_map, db_mngr, db_editor):
+        db_map.add_entity_class(name="Object")
+        db_map.add_entity(entity_class_name="Object", name="cube")
+        db_map.add_entity(entity_class_name="Object", name="sphere")
+        table_view = db_editor.ui.empty_entity_alternative_table_view
+        model = table_view.model()
+        entity_class_column = model.header.index("entity_class_name")
+        entity_byname_column = model.header.index("entity_byname")
+        model.batch_set_data([model.index(0, entity_class_column)], ["Object"])
+        delegate = EntityBynameDelegate(db_editor, db_mngr)
+        editor = delegate.createEditor(db_editor, QStyleOptionViewItem(), model.index(0, entity_byname_column))
+        assert isinstance(editor, GroupEditor)
+        assert editor.data() is ()
+        expected = [[""], ["cube"], ["sphere"]]
+        assert_table_model_data_pytest(editor.proxy_model, expected)
+
+    def test_create_editor_for_empty_byname_with_empty_model(self, db_map, db_mngr, db_editor):
+        db_map.add_entity_class(name="Object")
+        db_map.add_entity(entity_class_name="Object", name="cube")
+        db_map.add_entity(entity_class_name="Object", name="sphere")
+        table_view = db_editor.ui.empty_entity_alternative_table_view
+        model = table_view.model()
+        entity_class_column = model.header.index("entity_class_name")
+        entity_byname_column = model.header.index("entity_byname")
+        model.batch_set_data([model.index(0, entity_class_column)], ["Object"])
+        model.batch_set_data([model.index(0, entity_byname_column)], [()])
+        delegate = EntityBynameDelegate(db_editor, db_mngr)
+        editor = delegate.createEditor(db_editor, QStyleOptionViewItem(), model.index(0, entity_byname_column))
+        assert isinstance(editor, GroupEditor)
+        assert editor.data() == ()
+        expected = [[""], ["cube"], ["sphere"]]
+        assert_table_model_data_pytest(editor.proxy_model, expected)
+
+    def test_create_editor_for_preselected_0_dimensional_entity_with_empty_model(self, db_map, db_mngr, db_editor):
+        db_map.add_entity_class(name="Object")
+        db_map.add_entity(entity_class_name="Object", name="cube")
+        db_map.add_entity(entity_class_name="Object", name="sphere")
+        table_view = db_editor.ui.empty_entity_alternative_table_view
+        model = table_view.model()
+        entity_class_column = model.header.index("entity_class_name")
+        entity_byname_column = model.header.index("entity_byname")
+        model.batch_set_data([model.index(0, entity_class_column)], ["Object"])
+        model.batch_set_data([model.index(0, entity_byname_column)], [("cube",)])
+        delegate = EntityBynameDelegate(db_editor, db_mngr)
+        editor = delegate.createEditor(db_editor, QStyleOptionViewItem(), model.index(0, entity_byname_column))
+        assert isinstance(editor, GroupEditor)
+        assert editor.data() == ("cube",)
+        expected = [["cube"], ["cube"], ["sphere"]]
+        assert_table_model_data_pytest(editor.proxy_model, expected)
+
+    def test_create_editor_for_preselected_multi_dimensional_entity_with_empty_model(self, db_map, db_mngr, db_editor):
+        db_map.add_entity_class(name="Source")
+        db_map.add_entity(entity_class_name="Source", name="source")
+        db_map.add_entity_class(name="Target")
+        db_map.add_entity(entity_class_name="Target", name="target1")
+        db_map.add_entity(entity_class_name="Target", name="target2")
+        relationship = db_map.add_entity_class(dimension_name_list=["Source", "Target"])
+        db_map.add_entity(entity_class_name="Source__Target", entity_byname=("source", "target1"))
+        db_map.add_entity(entity_class_name="Source__Target", entity_byname=("source", "target2"))
+        table_view = db_editor.ui.empty_entity_alternative_table_view
+        model = table_view.model()
+        model.set_default_row(
+            entity_class_name="Source__Target", database=db_mngr.name_registry.display_name(db_map.db_url)
+        )
+        model.set_rows_to_default(0)
+        entity_byname_column = model.header.index("entity_byname")
+        model.batch_set_data([model.index(0, entity_byname_column)], [("source", "target1")])
+        delegate = EntityBynameDelegate(db_editor, db_mngr)
+        index = model.index(0, entity_byname_column)
+        with signal_waiter(delegate.element_name_list_editor_requested) as waiter:
+            editor = delegate.createEditor(db_editor, QStyleOptionViewItem(), index)
+        assert editor is None
+        assert waiter.args == (index, relationship["id"], db_map)
