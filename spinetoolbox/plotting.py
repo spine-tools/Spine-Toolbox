4242527764d1ca99bc30c7055fd58d2ba9e16955
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2026-02-25 15:18:31 +0100

    plotting: cleanup imports

--- spinetoolbox/plotting.py
+++ spinetoolbox/plotting.py
@@ -28,9 +28,9 @@ from bokeh.models import ColumnDataSource, FactorRange, HoverTool, Legend, Range
 from bokeh.palettes import TolRainbow
 from bokeh.plotting import figure
 import pandas as pd
-from typing import Dict, Iterable, List, Optional, TypeAlias, TypeVar, Union, cast
+from typing import Dict, Iterable, List, Literal, Optional, TypeVar, Union
 import numpy as np
-from PySide6.QtCore import QSize, Qt, QUrl
+from PySide6.QtCore import QSize, Qt
 from spinedb_api import DateTime, IndexedValue
 from spinedb_api.dataframes import to_dataframe
 from .mvcmodels.shared import PARAMETER_VALUE_ROLE, PARSED_ROLE
